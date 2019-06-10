b96861bb9aaee8f9e24c4e1ceb0f69678b33b79b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-06-10 23:40:54 +0600

    Добавляет анимацию

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,27 @@
   <div class="training">
     <h1>Math training</h1>
     <hr>
-    <app-start-screen
-      v-if="state == 'start'"
-      @onStart="onStart"
-    />
-    <app-question v-else-if="state == 'question'" />
-    <app-message v-else-if="state == 'message'" />
-    <app-result-screen v-else-if="state == 'results'" />
-    <!-- <div v-else>Unknown state</div> -->
+    <div class="box">
+      <transition name="flip" mode="out-in">
+      <app-start-screen
+        v-if="state == 'start'"
+        @onStart="onStart"
+      />
+      <app-question
+        v-else-if="state == 'question'"
+        @success="onQuestionSuccess"
+        @error="onQuestionError"
+        />
+      <app-message
+        v-else-if="state == 'message'"
+        :type="message.type"
+        :text="message.text"
+        @onNext="onNext"
+      />
+      <app-result-screen v-else-if="state == 'results'" />
+      <!-- <div v-else>Unknown state</div> -->
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -18,11 +31,38 @@ export default {
   name: 'app',
   data () {
     return {
-      state: 'start'
+      state: 'start',
+      stats: {
+        success: 0,
+        errors: 0
+      },
+      message: {
+        type: '',
+        text: ''
+      },
+      questionMax: 3
+    }
+  },
+  computed: {
+    questDone() {
+      return this.stats.success + this.stats.error;
     }
   },
   methods: {
     onStart () {
+      this.state = 'question';
+    },
+    onQuestionSuccess() {
+      this.state = 'message';
+      this.message.text = 'Good Job!';
+      this.message.type = 'success';
+    },
+    onQuestionError(msg) {
+      this.state = 'message';
+      this.message.text = msg;
+      this.message.type = 'warning';
+    },
+    onNext() {
       this.state = 'question';
     }
   }
@@ -33,5 +73,31 @@ export default {
   .training {
     max-width: 700px;
     margin: 20px auto;
+  }
+
+  .flip-enter {
+
+  }
+
+  .flip-enter-active {
+    animation: flipInX 0.3s linear;
+  }
+
+  .flip-leave {
+
+  }
+
+  .flip-leave-active {
+    animation: flipOutX 0.3s linear;
+  }
+
+  @keyframes flipInX {
+    from {transform: rotateX(90deg);}
+    to {transform: rotateX(0deg);}
+  }
+
+  @keyframes flipOutX {
+    from {transform: rotateX(0deg);}
+    to {transform: rotateX(90deg);}
   }
 </style>
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>
-    2
+  <div class="alert">
+    <h3>{{ x }} + {{ y }} = ?</h3>
+    <hr>
+    <div class="buttons">
+      <button
+        class="btn btn-success"
+        v-for="number in answers"
+        @click="onAnswer(number)"
+      >
+        {{ number }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      x: mtRand(10, 100),
+      y: mtRand(10, 100)
+    }
+  },
+  computed: {
+    good() {
+      return this.x + this.y;
+    },
+    answers() {
+      let res = [this.good];
+      while (res.length < 4) {
+        let num = mtRand(this.good - 5, this.good + 10);
+        if (res.indexOf(num) === -1) {
+          res.push(num);
+        }
+      }
+      return res.sort(function(a, b) {
+        return Math.random() - 0.5;
+      });
+    }
+  },
+  methods: {
+    onAnswer(num) {
+      if (num === this.good) {
+        this.$emit('success');
+      } else {
+        this.$emit('error', `${this.x} + ${this.y} = ${this.good}!`);
+      }
+    }
+  }
+};
+// Приватная функция
+function mtRand(min, max) {
+  let diff = max - min;
+  return Math.floor(Math.random() * (diff + 1)) + min;
 };
 </script>
 
 <style scoped>
-
+.alert {
+  padding-top: 20px;
+  background-color: #eeeeee;
+}
+.buttons {
+  display: flex;
+  justify-content: space-around;
+}
+.btn {
+  margin: 20px 0;
+}
 </style>
